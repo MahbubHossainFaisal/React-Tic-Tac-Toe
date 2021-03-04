b24f0c709839de3562399f259a7a4832d8484e9c
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-03-04 16:51:16 +0600

    Tic Tac Toe Game

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,30 +1,12 @@
-import React, { Component } from 'react'
-import Square from './Square'
+import React from "react";
+import Square from "./Square";
 
-export default class Board extends Component {
+const Board = ({ squares, onClick }) => (
+  <div className="board">
+    {squares.map((square, i) => (
+      <Square key={i} value={square} onClick={() => onClick(i)} />
+    ))}
+  </div>
+);
 
-    takeSquare = (input)=>{
-        return <Square value={this.props.squares[input]} onClick={()=>this.props.onClick(input)}
-    }
-    render() {
-        return (
-            <div>
-                <div className="border-row">
-                    {this.takeSquare(0)}
-                    {this.takeSquare(1)}
-                    {this.takeSquare(2)}
-                </div>
-                 <div className="border-row">
-                    {this.takeSquare(3)}
-                    {this.takeSquare(4)}
-                    {this.takeSquare(5)}
-                </div>
-                 <div className="border-row">
-                    {this.takeSquare(6)}
-                    {this.takeSquare(7)}
-                    {this.takeSquare(8)}
-                </div>
-            </div>
-        )
-    }
-}
+export default Board;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,52 @@
-import React, { Component } from 'react'
+import React, { useState } from "react";
+import { calculateWinner } from "./Winner";
+import Board from "./Board";
 
-export default class main extends Component {
-    constructor(props){
-        super(props)
-    }
-    render() {
-        return (
-            <div>
-                
-            </div>
+const Main = () => {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXisNext] = useState(true);
+  const winner = calculateWinner(history[stepNumber]);
+  const xO = xIsNext ? "X" : "O";
+
+  const handleClick = (i) => {
+    const historyPoint = history.slice(0, stepNumber + 1);
+    const current = historyPoint[stepNumber];
+    const squares = [...current];
+    if (winner || squares[i]) return;
+    squares[i] = xO;
+    setHistory([...historyPoint, squares]);
+    setStepNumber(historyPoint.length);
+    setXisNext(!xIsNext);
+  };
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXisNext(step % 2 === 0);
+  };
+
+  const renderMoves = () =>
+    {
+        return(
+            <li key={0}>
+            <button onClick={() => jumpTo(0)} style={{width:100,fontSize:20}}>Start Again</button>
+            </li>
         )
+    
     }
-}
+
+  return (
+    <>
+      <h1 style={{color:'black', fontSize:50}}>Tic Tac Toe</h1>
+      <Board squares={history[stepNumber]} onClick={handleClick} />
+      <div className="info">
+        <div>
+          {renderMoves()}
+        </div>
+        <h3 style={{fontSize:50}}>{winner ? "Winner: " + winner : "Next Player: " + xO}</h3>
+      </div>
+    </>
+  );
+};
+
+export default Main;
